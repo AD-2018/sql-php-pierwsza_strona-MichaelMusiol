dd8491651ec6169332ba943a8deca002c65631a5
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-10-29 15:06:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,35 +11,13 @@ echo("<h3>ZAD.1</h3>");
 $result = mysqli_query($conn, $sql);
 
 echo('<table border="1" class="tabela"');
-echo ("<tr><th>id</th><th>imie</th><th>dzial</th><th>zarobki</th></tr>");
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th></tr>");
 while($row=mysqli_fetch_assoc($result)){
 echo("<tr>");
-    //print_r($row);
     echo("<tr>");
-    echo("<td>".$row['id']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
-    echo("<tr>");
-echo("</tr>");
-
-}
-echo('</table>');
-echo("<h3>ZAD.2</h3>");
-
-$sql ="select * from pracownicy where imie not like '%a'";
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id</th><th>imie</th><th>dzial</th><th>zarobki</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    //print_r($row);
-    echo("<tr>");
-    echo("<td>".$row['id']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
     echo("<tr>");
 echo("</tr>");
-
 }
 echo('</table>');
-
-
-
 ?>
