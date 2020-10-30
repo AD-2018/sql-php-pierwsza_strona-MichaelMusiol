108e572b9eb4f465c27c5ef9584ccf2593c15055
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-10-30 11:50:43 +0100

    komunikat błędu

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ $dbname = "sql7373397";
 
 $conn= new mysqli($servername,$username,$password,$dbname);
 
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $sql ="select * from pracownicy";
 echo("<h3>ZAD.1</h3>");
 $result = mysqli_query($conn, $sql);
