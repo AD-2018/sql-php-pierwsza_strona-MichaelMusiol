68a10c8b739aaa16d726520dd7baa5797d8ec01f
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-06 11:30:27 +0100

    Update organizacjaPracownicy.php

--- organizacjaPracownicy.php
+++ organizacjaPracownicy.php
@@ -1,6 +1,17 @@
-<?php
-<div>
-<a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github  </a>
-<a href="funkcjeAgregujace.php">funkcjeAgregujace.php  </a>
-</div>
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">GŁÓWNA</a>
+    
+    <div class="nav">
+         <a href="index.php">Start</a>   
+         <a href="organizacjaPracownicy.php">Pracownicy i Organizacja</a>   
+         <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>   
+    </div>
+  
+</body>
+</html>
