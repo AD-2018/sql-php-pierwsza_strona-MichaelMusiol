8f893f43956554e558e18289291a394ad29c661b
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-06 11:42:54 +0100

    Update funkcjeAgregujace.php

--- funkcjeAgregujace.php
+++ funkcjeAgregujace.php
@@ -4,11 +4,13 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
     
     <div class="nav">
-         <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
-         <a href="index.php">Start</a>   
-         <a href="organizacjaPracownicy.php">Pracownicy i Organizacja</a>      
+         <a href="index.php">Pracownicy</a>
+         <a href="organizacjaPracownicy.php">Pracownicy i Organizacja</a>
+         <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>   
     </div>
   
 </body>
