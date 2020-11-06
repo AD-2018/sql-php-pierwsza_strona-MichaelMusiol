62bf11ca84f93ddc20c50066baa342d72513e3fa
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-06 12:04:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-Witaj Świecie    
+    <div>
+       <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol  ">Github</a>
+       <a href="organizacjaPracownicy.php  ">organizacjaPracownicy.php</a>
+       <a href="funkcjeAgregujace.php  ">funkcjeAgregujace.php</a>
+    </div>
 
 
 </body>
