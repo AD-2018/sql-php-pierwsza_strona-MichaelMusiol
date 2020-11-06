9b0f7be2465abb9c88a36d50ef6236f509f256b3
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-06 12:02:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,17 @@
 </head>
 <body>
     
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
-    
-    <div class="nav">
-         <a href="index.php">Pracownicy</a>
-         <a href="organizacjaPracownicy.php">Pracownicy i Organizacja</a>
-         <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>   
+    <div>
+       <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol  ">Github</a>
+       <a href="organizacjaPracownicy.php  ">organizacjaPracownicy.php</a>
+       <a href="funkcjeAgregujace.php  ">funkcjeAgregujace.php</a>
     </div>
 <?php
 echo("<h1>Michael Musiol nr. 8</h1>");
 
 require_once "lib.php";
 
-$sql = "SELECT * FROM pracownicy where dzial=2";
+$sql = "SELECT * FROM pracownicy";
 echo("<h3>ZADANIE 1</h3>");
 echo("<li>".$sql."<br><br>");
 
@@ -29,7 +27,7 @@ $result = mysqli_query($conn, $sql);
     }
 
 echo('<table border="1" class="tabelka_moja">');
-echo ("<tr><th>imie</th><th>zarobki</th><th>dzial</th><th>data urodzenia</th></tr>");
+echo ("<tr><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>dzial</th></tr>");
 while($row = mysqli_fetch_assoc($result)) {
     echo ('<tr>');
     echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['dzial']."</td><td>".$row['data_urodzenia']".</td>");
@@ -73,6 +71,46 @@ echo ("<tr><th>imie</th><th>zarobki</th><th>dzial</th><th>data urodzenia</th></t
 while($row = mysqli_fetch_assoc($result)) {
     echo ('<tr>');
     echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['dzial']."</td><td>".$row['data_urodzenia']".</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
+    
+        $sql = "SELECT imie,zarobki,data_urodzenia,nazwa_dzial FROM pracownicy,organizacja WHERE id_org=dzial";
+echo("<h3>ZADANIE 4</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>nazwa_dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['nazwa_dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
+
+$sql = "SELECT imie,zarobki,data_urodzenia,nazwa_dzial FROM pracownicy,organizacja WHERE id_org=dzial AND zarobki<30";
+echo("<h3>ZADANIE 5</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>nazwa_dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['nazwa_dzial']."</td>");
     echo ('</tr>');
 }
 echo ('</table>');
