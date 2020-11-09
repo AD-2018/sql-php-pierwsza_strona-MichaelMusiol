5ce2eddd03e2596e79a7439ea70ffb7fee41f08e
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-09 14:34:12 +0100

    Update organizacjaPracownicy.php

--- organizacjaPracownicy.php
+++ organizacjaPracownicy.php
@@ -12,6 +12,32 @@
          <a href="organizacjaPracownicy.php">Pracownicy i Organizacja</a>
          <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>   
     </div>
+    
+    <?php
+            echo("<br/><h3>Zad.1</h3><br/>");
+        $sql = "SELECT imie, dzial FROM pracownicy, organizacja WHERE dzial = id_org";
+        echo($sql);
+        
+        $result = mysqli_query($conn, $sql);
+        if ( $result) {
+                echo("<li>ok");
+            } else {
+              echo("Error: ".$sql."<br>". mysqli_error($conn));
+            }
+        
+        echo('<table border="1">');
+            echo('<th>id</th><th>imie</th><th>zarobki</th><th>dzial</th><th>nazwa dzial</th><th>data urodzenia</th>');
+        
+            while($row=mysqli_fetch_assoc($result)){
+                echo('<tr>');
+                echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td><td>'.$row['nazwa_dzial'].'</td><td>'.$row['data_urodzenia'].'</td>');
+                echo('</tr>');
+            }
+        
+            echo('</table>');
+    
+    
+    ?>
   
 </body>
 </html>
