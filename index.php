ae41153defb5ee29fdd605b3a40ac8085f834fc7
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-09 14:16:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 <?php
 require_once("lib.php");
-echo("<h1>Michael Musiol nr. 8</h1>");
+echo("<h1>Michael Musiol nr.8</h1>");
 
 echo("<br/><h3>Zad.1</h3><br/>");
 $sql = "SELECT * FROM pracownicy where dzial=2";
