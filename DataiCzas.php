55d07291fc4c7a2849b5d20e0ce8b201960e5aa0
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-12 14:04:12 +0100

    Create DataiCzas.php

---
+++ DataiCzas.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
+    <a href="Pracownicyorganizacja.php">Organizacja Pracownicy</a>
+    <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>
+    <a href="DataiCzas.php">Data i Czas</a>
+    
+
+<?php
+require_once("lib.php");
+echo("<h1>Michael Musiol nr.8</h1>");
+
+echo("<h3>Zad.1</h3>");
+$sql = "SELECT * FROM pracownicy";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo("<li>ok");
+    } else {
+      echo("Error: ".$sql."<br/><br/>". mysqli_error($conn));
+    }
+
+echo('<table border="1">');
+    echo('<th>id</th><th>imie</th><th>zarobki</th><th>dzial</th><th>data urodzenia</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td><td>'.$row['data_urodzenia'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+        ?>
+
+</body>
+</html>
