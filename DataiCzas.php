b19e8634a8c6622b5879a7087614935756bcf5fc
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-13 12:56:17 +0100

    Update DataiCzas.php

--- DataiCzas.php
+++ DataiCzas.php
@@ -336,7 +336,7 @@ echo('<table border="1">');
 
     
 $sql = "SELECT imie,zarobki,dzial, DATE_FORMAT(data_urodzenia,'%W-%m-%Y') as data_urodzenia from pracownicy;";
-echo("<h3>ZADANIE 1</h3>");
+echo("<h3>Zad.1</h3>");
 echo("<li>".$sql."<br><br>");
 
 $result = mysqli_query($conn, $sql);
@@ -355,7 +355,7 @@ while($row = mysqli_fetch_assoc($result)) {
 }
 echo ('</table>');
 
-echo("<h3> ZADANIE 2 </h3>");
+echo("<h3>Zad.2</h3>");
     $sql1 = "SET lc_time_names = 'pl_PL'";
     $sql2 ="SELECT DATE_FORMAT(CURDATE(), '%W')as data";
     echo ("<li>".$sql2."</li><br><br>");
@@ -372,7 +372,7 @@ echo("<h3> ZADANIE 2 </h3>");
 echo('</table>');
     
 $sql = "SELECT imie,zarobki,dzial, DATE_FORMAT(data_urodzenia,'%w-%M-%Y') as data_urodzenia from pracownicy;";
-echo("<h3>ZADANIE 3</h3>");
+echo("<h3>Zad.3</h3>");
 echo("<li>".$sql."<br><br>");
 
 $result = mysqli_query($conn, $sql);
@@ -392,7 +392,7 @@ while($row = mysqli_fetch_assoc($result)) {
 echo ('</table>');
     
 $sql = "SELECT curtime(4) as czas";
-echo("<h3>ZADANIE 4</h3>");
+echo("<h3>Zad.4</h3>");
 echo("<li>".$sql."<br><br>");
 
 $result = mysqli_query($conn, $sql);
@@ -412,7 +412,7 @@ while($row = mysqli_fetch_assoc($result)) {
 echo ('</table>');
     
 $sql = "SELECT imie,zarobki,dzial, DATE_FORMAT(data_urodzenia,'%Y-%M-%W') as data_urodzenia from pracownicy;";
-echo("<h3>ZADANIE 5</h3>");
+echo("<h3>Zad.5</h3>");
 echo("<li>".$sql."<br><br>");
 
 $result = mysqli_query($conn, $sql);
@@ -432,7 +432,7 @@ while($row = mysqli_fetch_assoc($result)) {
 echo ('</table>');
     
 $sql = "SELECT imie,DATEDIFF(CURDATE(),data_urodzenia) as dni, DATEDIFF(CURDATE(),data_urodzenia)*24 as godziny, DATEDIFF(CURDATE(),data_urodzenia)*24*60 as minuty FROM pracownicy";
-echo("<h3>ZADANIE 6</h3>");
+echo("<h3>Zad.6</h3>");
 echo("<li>".$sql."<br><br>");
 
 $result = mysqli_query($conn, $sql);
@@ -451,7 +451,7 @@ while($row = mysqli_fetch_assoc($result)) {
 }
 echo ('</table>');
     
-echo("<h3>ZADANIE 7</h3>");
+echo("<h3>Zad.7</h3>");
 $sql = 'SELECT DATE_FORMAT("2002-04-23", "%j") as urodz';
 echo($sql);
 
@@ -487,7 +487,7 @@ ORDER BY
           WHEN dzien = 'Sobota' THEN 6
           WHEN dzien = 'Niedziela' THEN 7
      END ASC";
-echo("<h3>ZADANIE 8</h3>");
+echo("<h3>Zad.8</h3>");
 echo("<li>".$sql."<br><br>");
 
 $result = mysqli_query($conn, $sql);
@@ -507,7 +507,7 @@ while($row = mysqli_fetch_assoc($result)) {
 echo ('</table>');
     
 $sql = "SELECT count(DATE_FORMAT(data_urodzenia, '%W')) as lp from pracownicy, organizacja where id_org=dzial and (DATE_FORMAT(data_urodzenia, '%W')='Poniedziałek');";
-echo("<h3>ZADANIE 9</h3>");
+echo("<h3>Zad.9</h3>");
 echo("<li>".$sql."<br><br>");
 
 $result = mysqli_query($conn, $sql);
@@ -526,7 +526,7 @@ while($row = mysqli_fetch_assoc($result)) {
 }
 echo ('</table>');
     
-echo("<h3>ZADANIE 10</h3>");
+echo("<h3>zad.10</h3>");
 $sql = "SELECT DATE_FORMAT(data_urodzenia,'%W') as dzien, count(date_format(data_urodzenia, '%W')) as Liczba FROM pracownicy GROUP BY dzien ORDER BY CASE
           WHEN dzien = 'Poniedziałek' THEN 1
           WHEN dzien = 'Wtorek' THEN 2
