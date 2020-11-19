9dd6c11a21128f51070e09cc710a191f940f738d
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-19 14:00:21 +0100

    Update funkcjeAgregujace.php

--- funkcjeAgregujace.php
+++ funkcjeAgregujace.php
@@ -10,6 +10,7 @@
     <a href="Pracownicyorganizacja.php">Organizacja Pracownicy</a>
     <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>
     <a href="DataiCzas.php">Data i Czas</a>
+    <a href="index.html">Strona(formularz)</a>
     
 <?php
 require_once("lib.php");
