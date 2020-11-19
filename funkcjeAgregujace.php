16838e4998e8f2b6c9bcd7e5ad2a623ae7785b84
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-19 14:10:24 +0100

    Update funkcjeAgregujace.php

--- funkcjeAgregujace.php
+++ funkcjeAgregujace.php
@@ -11,6 +11,7 @@
     <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>
     <a href="DataiCzas.php">Data i Czas</a>
     <a href="index.html">Strona(formularz)</a>
+    <a href="index.html">Strona(insert)</a>
     
 <?php
 require_once("lib.php");
