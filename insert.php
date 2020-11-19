b1eff2cf5cc94c84380181594676085cb9c0165b
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-19 14:29:53 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -22,7 +22,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO Pracownicy VALUES (17,'Ksawery', 3, 36,'1995-10-21')";
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki,data_urodzenia)
+VALUES (17,'Ksawery', 3, 36,'1995-10-21');";
 
 $conn->query($sql);
 
