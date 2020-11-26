f7db7c52d5916ee9d04095334be86acb6075afdd
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-26 14:14:02 +0100

    Update usuwanie.php

--- usuwanie.php
+++ usuwanie.php
@@ -5,6 +5,7 @@
 </head>
 <body>
     
+    
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
     <a href="index.php">Pracownicy</a>
     <a href="Pracownicyorganizacja.php">Organizacja Pracownicy</a>
