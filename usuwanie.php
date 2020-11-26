3c69161e62a769807aeed1c4095dc562b8d45136
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-26 13:56:07 +0100

    Create usuwanie.php

---
+++ usuwanie.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
+    <a href="index.php">Pracownicy</a>
+    <a href="Pracownicyorganizacja.php">Organizacja Pracownicy</a>
+    <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>
+    <a href="DataiCzas.php">Data i Czas</a>
+    <a href="index.html">Strona(form)</a>
+    <a href="insert.php">Strona(insert)</a>
+    <a href="daneDoBazy.html">daneDoBazy</a> 
+    
+
+<?php
+require_once("lib.php");
+
+?>
+
+</body>
+</html>
