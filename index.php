9a4b49b4ebc126e9597e5ae70a06103f401ae50f
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-27 11:48:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <a href="Pracownicyorganizacja.php">Organizacja Pracownicy</a>
     <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>
     <a href="DataiCzas.php">Data i Czas</a>
-    <br/><br/><br/>
+    <br/><br/><br/><br/>
     <a href="index.html">Strona(form)</a>
     <a href="insert.php">Strona(insert)</a>
     <a href="daneDoBazy.html">daneDoBazy</a> 
