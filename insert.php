9a530060d459f7bc40f0c1cf4d130c6b192032e6
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-28 17:42:35 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -16,16 +16,33 @@
 
 <?php
 require_once("lib.php");
-
+echo("jestes w insert.php <br>");
+echo "<li>". $_POST['imie'];
+echo "<li>". $_POST['dzial'];
+echo "<li>". $_POST['zarobki'];
+echo "<li>". $_POST['data_urodzenia'];
+    
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
+$sql = "INSERT INTO Pracownik (null, imie, dzial,zarobki,data_urodzenia)
+	      VALUES (
+					null,". 
+					$_POST['imie'], 
+					$_POST['dzial'], 
+					$_POST['zarobki'],
+					$_POST['data_urodzenia']
+				;
 
-$conn->query($sql);
+echo "<li>". $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 ?>
