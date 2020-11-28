020fbc58c4e54be789320177224d38ecca3ab65b
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-28 19:37:06 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -16,18 +16,19 @@
 
 <?php
 require_once("lib.php");
-echo("<br/>jestes w insert.php <br/>");
+echo("<br/><br/>jestes w insert.php <br/>");
 echo "<li>". $_POST['imie'];
 echo "<li>". $_POST['dzial'];
 echo "<li>". $_POST['zarobki'];
 echo "<li>". $_POST['data_urodzenia'];
     
+    $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+    VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
     $puste = mysqli_query($conn,"SELECT * FROM pracownicy WHERE imie = ''";
     if($puste==TRUE)){
     $_POST['imie']="Ksawery";
     }
-    $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+
     
   echo "<br>New record created successfully";
     
