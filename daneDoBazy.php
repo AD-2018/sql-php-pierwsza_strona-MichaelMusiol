f9e9606bde97a8183d12bea99026e9b7938bd42c
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-28 17:35:41 +0100

    Update daneDoBazy.php

--- daneDoBazy.php
+++ daneDoBazy.php
@@ -17,11 +17,11 @@
  
     <h3>DODAJ PRACOWNIKA</h3>	
 	<form action="insert.php" method="POST">
-	    <label>Imię </label><input type="text" name="imie"></br>
-		<label>Dział </label><input type="number" name="dzial"></br>
-		<label>Zarobki </label><input type="number" name="zarobki"></br>
-		<input type="date" name="data_urodzenia"></br>
-	    <input type="submit" value="dodaj pracownika">
+	<label>Imię </label><input type="text" name="imie"></br>
+	<label>Dział </label><input type="number" name="dzial"></br>
+	<label>Zarobki </label><input type="number" name="zarobki"></br>
+	<input type="date" name="data_urodzenia"></br>
+	<input type="submit" value="dodaj pracownika">
 	</form>
     
 
@@ -42,16 +42,16 @@
      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
      }
     echo('<table border="1">');
-        echo('<th>Id_pracownika</th><th>imię</th><th>dział</th><th>zarobki</th><th>data urodzenia</th>');
+        echo('<th>Id_pracownika</th><th>imię</th><th>dział</th><th>zarobki</th><th>data urodzenia</th><th>Usuń</th>');
         while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
         echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td>'.'<td>
-	    <form action="delete.php" method="POST">
-  		<input type="number" name="id" value="'.$row['id_pracownicy'].'" hidden>
-   		<input type="submit" value="X">
+	<form action="delete.php" method="POST">
+  	<input type="number" name="id" value="'.$row['id_pracownicy'].'" hidden>
+	<input type="submit" value="X">
 	</form>
 	</td>');
-		echo('</tr>');
+	echo('</tr>');
      }
         echo('</table>');
 ?>
