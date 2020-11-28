cb05856e5e74a064d44af2f4f113d27bb32442a3
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-11-28 17:34:08 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -19,12 +19,13 @@ $sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id'];
 
 //wyświetlamy zapytanie $sql
 echo $sql;
-
+header('Location: https://dyljoanna.herokuapp.com/daneDoBazy.php');
 if ($conn->query($sql) === TRUE) {
   echo "Record deleted successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
+ 
 
 $conn->close();
 ?>
