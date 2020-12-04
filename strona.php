d6cdbd1e666898eb19907b4dcc35640980995224
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-12-04 12:01:46 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
+    <a href="index.php">Pracownicy</a>
+    <a href="Pracownicyorganizacja.php">Organizacja Pracownicy</a>
+    <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>
+    <a href="DataiCzas.php">Data i Czas</a>
+    <br/><br/><br/>
+    <a href="ProstyFormularz.php">Formularz</a>
+    <a href="insert.php">Insert</a>
+    <a href="daneDoBazy.php">Dane Do Bazy</a> 
+    
 <?php
 	echo("jesteś na stronie.php");
 	
@@ -7,3 +23,5 @@
 		
 	echo("<ul>");
 ?>
+	</body>
+</html>
