d897064d8448a19d9e9367d375833843770f5e75
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-12-04 11:59:57 +0100

    Update ProstyFormularz.php

--- ProstyFormularz.php
+++ ProstyFormularz.php
@@ -9,7 +9,7 @@
   <?php 
   require_once("lib.php");
   ?>
-<a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
     <a href="index.php">Pracownicy</a>
     <a href="Pracownicyorganizacja.php">Organizacja Pracownicy</a>
     <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>
@@ -17,8 +17,9 @@
     <br/><br/><br/>
     <a href="ProstyFormularz.php">Formularz</a>
     <a href="insert.php">Insert</a>
-    <a href="daneDoBazy.php">Dane Do Bazy</a>
+    <a href="daneDoBazy.php">Dane Do Bazy</a> 
     <br/><br/><br/>
+    <a href="ksiazki.php">Książki</a>
 <form action="strona.php" method="POST">
    <input type="text" name="firstname"><br/>
    <input type="submit" value="wyślij do strona.php">
