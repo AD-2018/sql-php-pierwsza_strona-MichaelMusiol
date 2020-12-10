ae9f15c9461c455122f852c90bd2d75c77bdb591
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-12-10 14:56:23 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -50,11 +50,11 @@ echo("<h3>Połączone</h3>");
 $sql = "SELECT * FROM biblAutor_biblTytuł,biblTytuł,biblAutor where id_tytuł=biblTytuł_id AND id_autor=biblAutor_id order by id_tytuł";
     $result = mysqli_query($conn, $sql);
         echo('<table border="1">');
-    echo('<th>id_tytuł</th><th>Autor</th><th>Tytuł</th>');
+    echo('<th>id</th><th>Autor</th><th>Tytuł</th>');
 
     while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
-        echo('<td>'.$row['id_tytuł'].'</td><td>'.$row['autor'].'</td><td>'.$row['tytuł'].'</td>');
+        echo('<td>'.$row['id'].'</td><td>'.$row['autor'].'</td><td>'.$row['tytuł'].'</td>');
         echo('</tr>');
     }
     echo('</table>');
