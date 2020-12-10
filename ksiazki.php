0a8e15b1073be2a7951d11837d10d0fe3209eb70
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2020-12-10 14:05:19 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -47,7 +47,7 @@ $sql = "SELECT * FROM biblTytuł";
 
     echo('</table>');
 echo("<h3>Połączone</h3>");
-$sql = "SELECT * FROM biblAutor_biblTytuł,biblTytuł,biblAutor where biblAutor.id_autorbibl=Autor_biblTytuł.biblAutor_id and biblAutor_biblTytuł.biblTytuł_id=biblTytuł.id_tytuł";
+$sql = "SELECT * FROM biblAutor_biblTytuł,biblTytuł,biblAutor where biblAutor.id_autor=biblAutor_biblTytuł.biblAutor_id and biblAutor_biblTytuł.biblTytuł_id=biblTytuł.id_tytuł";
     $result = mysqli_query($conn, $sql);
         echo('<table border="1">');
     echo('<th>id_autor</th><th>Autor</th><th>Tytuł</th>');
