f3f916e7f33e681ab18658e81b448ac2d605cec3
MichaelMusiol1 <michaelmusiol77@gmail.com>
2021-01-30 18:55:50 +0100

    flexbox

--- index.php
+++ index.php
@@ -6,20 +6,21 @@
 </head>
 <body>
 <div class="calosc">
-    <div class="tytul red">
+    <div class="tytul red"><br/>
 <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
     <div class="imie">
     <h1>Michael Musiol nr. 8</h1>
     </div>
     </div>
     <div class="menu blue">
-<a href="index.php">Pracownicy</a>
-    <a href="pracownicy/Pracownicyorganizacja.php">Organizacja Pracownicy</a>
-    <a href="pracownicy/funkcjeAgregujace.php">Funkcje Agregujace</a>
-    <a href="pracownicy/DataiCzas.php">Data i Czas</a>
-    <a href="pracownicy/ProstyFormularz.php">Formularz</a>
-    <a href="pracownicy/daneDoBazy.php">Dane Do Bazy</a> 
-    <a href="ksiazki/ksiazki.php">Książki</a>
+    <br/>
+<a href="index.php">Pracownicy</a><br/><br/><br/>
+    <a href="pracownicy/Pracownicyorganizacja.php">Organizacja</a><br/><br/><br/>
+    <a href="pracownicy/funkcjeAgregujace.php">Funkcje</a><br/><br/><br/>
+    <a href="pracownicy/DataiCzas.php">Data i Czas</a><br/><br/><br/>
+    <a href="pracownicy/ProstyFormularz.php">Formularz</a><br/><br/><br/>
+    <a href="pracownicy/daneDoBazy.php">Dane Do Bazy</a> <br/><br/><br/>
+    <a href="ksiazki/ksiazki.php">Książki</a><br/><br/><br/>
     </div>
     <div class="tresc green">
 
