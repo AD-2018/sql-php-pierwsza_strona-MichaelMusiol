95faf2c2e1df487d3e978810e3f945dd43768a33
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2021-02-08 19:18:43 +0100

    Update delete.php

--- pracownicy/delete.php
+++ pracownicy/delete.php
@@ -10,10 +10,6 @@ require_once("../lib.php");
 echo("jestes w delete.php <br>");
 echo $_POST['id'];
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
 //definiujemy zapytanie $sql
 $sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id'];
