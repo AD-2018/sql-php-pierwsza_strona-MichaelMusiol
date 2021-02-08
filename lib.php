a4131b01cf861ec77e1ce159b948daa8973e2b42
Michael Musiol <63040384+MichaelMusiol1@users.noreply.github.com>
2021-02-08 18:46:15 +0100

    Update lib.php

--- lib.php
+++ lib.php
@@ -1,6 +1,11 @@
 <?php
 
-$conn= new mysqli($servername,$username,$password,$dbname);
+$NazwaServera = $_SERVER["servername"];
+$NazwaUzyt = $_SERVER["username"];
+$Nazwahasla = $_SERVER["password"];
+$Nazwabazz = $_SERVER["dbname"];
+
+$conn= new mysqli($NazwaServera,$NazwaUzyt,$Nazwahasla,$Nazwabazz);
 
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
